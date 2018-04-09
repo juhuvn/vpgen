6d2bca0f65a8e8ce0d24198446991e710cc72fca
Cuong Nguyen <juhuvn@gmail.com>
2018-04-09 20:02:44 +0700

    Revert "update"
    This reverts commit d189c8caa2d86a3b32162a81a638688088004a4b.

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,6 @@
 |
 */
 
-
-Auth::routes();
-
-Route::group(['middleware' => 'auth'], function(){
-	// Route::get('/', function () {
- //    return view('welcome');
-	// });
-
-	Route::get('/', 'HomeController@index')->name('home');
-	
-
-	Route::get('/user', 'Admin\UserController@index')->name('user');
-	Route::get('/user/edit/{id}', 'Admin\UserController@edit')->name('user.edit');
-	Route::post('/user/edit/{id}', 'Admin\UserController@update')->name('user.update');
-
-	Route::get('/disease', 'DiseaseController@index')->name('disease');
-	Route::get('/disease/edit/{id}', 'DiseaseController@edit')->name('disease.edit');
-	Route::get('/disease/detail/{id}', 'DiseaseController@detail')->name('disease.detail');
-	Route::post('/disease/edit/{id}', 'DiseaseController@update')->name('disease.update');	
-
-	Route::get('/gene', 'DiseaseController@index')->name('gene');
-	Route::get('/gene/edit/{id}', 'DiseaseController@edit')->name('gene.edit');
-	Route::post('/gene/edit/{id}', 'DiseaseController@update')->name('gene.update');	
+Route::get('/', function () {
+    return view('welcome');
 });
